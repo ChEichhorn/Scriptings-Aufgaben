8dc47c5dd19497d2bbe46e31e5852992f0d71287
Christoph Christian Eichhorn <Christoph@Christoph-Christian-Eichhorns-MacBook-Pro.local>
2012-11-14 11:26:21 +0100

    Ich habe meine Aufgabe gemacht :)

---
+++ Semester-Objekt/Semester.js
@@ -0,0 +1,53 @@
+// ========================================
+// = Aufgabe: Semesterbelegung als Objekt =
+// ========================================
+
+var mein_semester = {
+  student: {
+    name: "Christoph Christian Eichhorn",
+    studiensemester: 3,
+	matrikelnummer: 901252,
+	studiengang: Interface Design,
+	studienabschluss: Master of Arts
+  },
+  welches: "WS",
+  jahr: "2012/13",
+  veranstaltungen: [ 
+    {
+      titel: "Scriptings",
+      sws: 2,
+      credits: 3,
+      workload: 90
+    },
+    {
+      titel: "poetische Apparate",
+      sws: 2,
+      credits: 3,
+      workload: 90
+    },
+	{
+      titel: "Gebe-Wesen",
+      sws: 2,
+      credits: 15,
+      workload: 450
+    },
+	{
+      titel: "Rhino 3D",
+      sws: 2,
+      credits: 2,
+      workload: 90
+    },
+	{
+      titel: "Read and Write for Design",
+      sws: 2,
+      credits: 3,
+      workload: 90
+    },
+	{
+      titel: "Scriptings",
+      sws: 2,
+      credits: 3,
+      workload: 90
+    }
+  ]
+};
